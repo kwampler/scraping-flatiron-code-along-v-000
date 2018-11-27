f128f98ed9a9c0e88d3e2367b13b2c5d69a88b57
Kate Preston <katejsenger@yahoo.com>
2018-11-27 03:53:00 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -17,6 +17,15 @@ class Scraper
       #course.description = post.css("p").text
     #end
   end
+  
+  def make_courses
+  self.get_courses.each do |post|
+    course = Course.new
+    course.title = post.css("h2").text
+    course.schedule = post.css(".date").text
+    course.description = post.css("p").text
+  end
+end
  
 end
 
